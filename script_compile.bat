5282f986b83df903cd55594732395cfa12687968
We1l6.f <we1l6.f@gmail.com>
2023-12-11 12:16:19 +0200

    first commit

---
+++ script_compile.bat
@@ -0,0 +1,7 @@
+cd build
+cmake --build . --config Release
+cd Release
+cls
+main.exe
+
+
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int i = 0;
+int main() {
+	std::cout << "GGGGGG";
+	std::cin >> i;
+	return 0;
+}
